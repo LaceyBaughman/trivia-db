b7dc5ed6dd34675cb201da0bbc1e8cdf6ba56aa3
LaceyBaughman <laceyebaughman@gmail.com>
2022-02-21 15:51:20 -0700

    Trivia working

--- app/Services/QuestionsService.js
+++ app/Services/QuestionsService.js
@@ -4,7 +4,7 @@ import { Trivia } from "../Models/Trivia.js";
 class QuestionsService {
   async getTrivia() {
     // @ts-ignore
-    const response = await axios.get('https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=boolean')
+    const response = await axios.get('https://opentdb.com/api.php?amount=1')
     console.log('response data', response.data)
     let trivia = response.data.results.map(t => new Trivia(t))
     ProxyState.trivia = trivia
